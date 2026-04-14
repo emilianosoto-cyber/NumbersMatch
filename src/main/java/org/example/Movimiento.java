dba15dbd50422f6590e5380a2adc93bb2533366b
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-04-13 22:09:01 -0700

    Se implementa la lógica para la clase Movimiento, encargada de guardar un movimiento para hacer la operación de deshacer o undo, se aplico de la misma manera anteriormente en la práctica de solitario.

--- src/main/java/org/example/Movimiento.java
+++ src/main/java/org/example/Movimiento.java
@@ -1,4 +1,40 @@
 package org.example;
 
+/**
+ * Guarda un movimiento para la operación deshacer.
+ */
 public class Movimiento {
+    private Casilla casilla1;
+    private Casilla casilla2;
+    private int puntosAntes;
+    private int encontradasAntes;
+    private int pendientesAntes;
+
+    public Movimiento(Casilla casilla1, Casilla casilla2, int puntosAntes, int encontradasAntes, int pendientesAntes) {
+        this.casilla1 = casilla1;
+        this.casilla2 = casilla2;
+        this.puntosAntes = puntosAntes;
+        this.encontradasAntes = encontradasAntes;
+        this.pendientesAntes = pendientesAntes;
+    }
+
+    public Casilla getCasilla1() {
+        return casilla1;
+    }
+
+    public Casilla getCasilla2() {
+        return casilla2;
+    }
+
+    public int getPuntosAntes() {
+        return puntosAntes;
+    }
+
+    public int getEncontradasAntes() {
+        return encontradasAntes;
+    }
+
+    public int getPendientesAntes() {
+        return pendientesAntes;
+    }
 }
