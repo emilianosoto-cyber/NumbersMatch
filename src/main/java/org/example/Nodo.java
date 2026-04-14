fa65b844396e51eae986a26edfe7794fd1c7b346
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-04-13 22:04:04 -0700

    Se utiliza como base la clase Nodo dentro de las instrucciones de la tarea pero, se modifica para el uso de la actividad.

--- src/main/java/org/example/Nodo.java
+++ src/main/java/org/example/Nodo.java
@@ -1,4 +1,31 @@
 package org.example;
 
-public class Nodo {
+/**
+ * Nodo simple genérico.
+ * Sirve como base para la lista simple.
+ */
+public class Nodo<T> {
+    private T info;
+    private Nodo<T> sig;
+
+    public Nodo(T info) {
+        this.info = info;
+        this.sig = null;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Nodo<T> getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo<T> sig) {
+        this.sig = sig;
+    }
 }
